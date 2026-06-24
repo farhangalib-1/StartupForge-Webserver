7ad12570c5cbc38b394d5bc91e8fa5c2d0d999b0
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-06-25 03:59:37 +0600

    Mongdb server is implemented and GET and POST api is added

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+
+    
+    const db = client.db("StartupForge")
+    const startupCollections = db.collection("startups")
+    app.post("/api/startups", async(req, res)=>{
+        const body = req.body
+        const result = await startupCollections.insertOne(body)
+        res.send(result)
+
+    })
+    app.get("/api/my-startups", async(req, res)=>{
+        const cursor = await startupCollections.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+  //await client.connect();
+  //await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("Server is running successfully 🚀");
+});
+
+app.get("/api/test", (req, res) => {
+  res.json({
+    success: true,
+    message: "API working perfectly",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
